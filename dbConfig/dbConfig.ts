474cf077d52f9c7329914c524bc110383e77a268
nitish singh <nitishsingh925@gmail.com>
2024-03-23 22:18:20 +0530

    add db Congfig file

---
+++ dbConfig/dbConfig.ts
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI!);
+
+    const dbConnection = mongoose.connection;
+
+    dbConnection.on("connected", () => {
+      console.log("MongoDB connected");
+    });
+
+    dbConnection.on("disconnected", () => {
+      console.log("MongoDB disconnected");
+    });
+
+    dbConnection.on("error", (err) => {
+      console.error("MongoDB connection error:", err);
+      process.exit(1);
+    });
+
+    return dbConnection;
+  } catch (error) {
+    console.error("Error in connecting to DB:", error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
